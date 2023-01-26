9b613fdea2085dbbb4b8a9c9752cb62d9ccc9458
harshgurehiya <harsh.gurehiya@gmail.com>
2023-01-26 16:12:42 +0530

    updated

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
 
-  const response = await fetch("https://codeh-24g3.onrender.com", {
+  const response = await fetch("https://codeh-gt9d.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
